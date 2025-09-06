078c4e4857a971153a4286b371b708a08cc071f5
Prince Kumar <pk7411956419@gmail.com>
2025-09-06 16:56:58 +0530

    commit

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Project from './pages/Project'
+import Projects from './pages/projects'
 
 function App() {
   return (
     <>
       <div>
-        <Project/>
+        <Projects/>
       </div>
     </>
   )
